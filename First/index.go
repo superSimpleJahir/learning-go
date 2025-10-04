5a50e32aca7280dd158a5b2250b226391b07f285
Jahir <mrjahir207@gmail.com>
2025-10-04 22:42:10 +0600

    learning function with go

--- First/index.go
+++ First/index.go
@@ -1,12 +1,17 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"rsc.io/quote"
-)
+func getNumbers(num1 int, num2 int) (int, int) {
+	sum := num1 + num2
+	diff := num1 - num2
+	return sum, diff
+}
 
 func main() {
-	fmt.Println("hello world")
-	fmt.Println(quote.Go())
+	sum, diff := getNumbers(5, 3)
+
+	fmt.Println("Sum:", sum)
+	fmt.Println("diff:", diff)
+
 }
